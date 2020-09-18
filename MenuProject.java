61e18ccbdb8d6f632ac5abf458d7f2780eb425f4
chill198929 <70107145+chill198929@users.noreply.github.com>
2020-09-17 18:54:02 -0600

    Add files via upload

---
+++ MenuProject.java
@@ -0,0 +1,119 @@
+
+package menuproject;
+
+import java.util.Scanner;
+
+public class MenuProject {
+
+    
+    public static void main(String[] args) {
+        
+        int choice = 0;
+        Scanner input = new Scanner(System.in);
+        String size = " ";
+        int drinkQuantity = 0;
+        double totalPrice = 0.0;
+        double drinkTotal = 0;
+        
+        
+      
+        
+            System.out.println("Coco's Cafe");
+        System.out.println("Press 1: Water");
+        System.out.println("Small: $0.00, Medium: $0.00, Large: $0.00");
+        System.out.println("Press 2: Tea");
+        System.out.println("Small: $1.50, Medium: $2.50, Large: $3.50");
+        System.out.println("Press 3: Milk");
+        System.out.println("Small: $1.00, Medium: $2.00, Large: $3.00");
+        System.out.println("Press 4: Lemonade");
+        System.out.println("Small: $2.00, Medium: $3.00, Large: $4.00");
+        System.out.println("Press 5: Latte");
+        System.out.println("Small: $3.50, Medium: $4.50, Large: $5.50");
+        System.out.println("Press 6: Mocha");
+        System.out.println("Small: $3.00, Medium: $4.00, Large: $5.00");
+        System.out.println("Press 7: Iced Coffe");
+        System.out.println("Small: $2.50, Medium: $3.50, Large: $4.50");
+        System.out.println("Press 8: Granita");
+        System.out.println("Small: $4.00, Medium: $5.00, Large: $6.00");
+        System.out.println("Press 9: Coffee");
+        System.out.println("Small: $1.00, Medium: $2.00, Large: $3.00");
+        System.out.println("Press 10: Chai Tea");
+        System.out.println("Small: $2.50, Medium: $3.50, Large: $4.50");
+        System.out.println("Press 0 to Exit and get Total ");
+        choice = input.nextInt();
+            
+        while(choice != 0)
+        {
+            switch(choice){
+                case 1:
+                    
+                    System.out.println("Not Implemented");
+                   break;
+                                
+                case 2:
+                    System.out.println("Not Implemented");
+                   break;
+                case 3:
+                   System.out.println("Not Implemented");
+                   break;
+                case 4:
+                   System.out.println("Not Implemented");
+                   break;
+                case 5:
+                   System.out.println("Not Implemented");
+                   break;
+                case 6:
+                   System.out.println("Not Implemented");
+                   break;
+                case 7:
+                    System.out.println("Not Implemented");
+                   break;
+                case 8:
+                    System.out.println("Not Implemented");
+                   break;
+                case 9:
+                    System.out.println("Not Implemented");
+                   break;
+                case 10:
+                    System.out.println("Not Implemented");
+                   break;
+                default:
+                        System.out.println("Invalid option, please enter a value 0-10");
+                        break;
+                        
+            }
+            
+        System.out.println("Coco's Cafe");
+        System.out.println("Press 1: Water");
+        System.out.println("Small: $0.00, Medium: $0.00, Large: $0.00");
+        System.out.println("Press 2: Tea");
+        System.out.println("Small: $1.50, Medium: $2.50, Large: $3.50");
+        System.out.println("Press 3: Milk");
+        System.out.println("Small: $1.00, Medium: $2.00, Large: $3.00");
+        System.out.println("Press 4: Lemonade");
+        System.out.println("Small: $2.00, Medium: $3.00, Large: $4.00");
+        System.out.println("Press 5: Latte");
+        System.out.println("Small: $3.50, Medium: $4.50, Large: $5.50");
+        System.out.println("Press 6: Mocha");
+        System.out.println("Small: $3.00, Medium: $4.00, Large: $5.00");
+        System.out.println("Press 7: Iced Coffe");
+        System.out.println("Small: $2.50, Medium: $3.50, Large: $4.50");
+        System.out.println("Press 8: Granita");
+        System.out.println("Small: $4.00, Medium: $5.00, Large: $6.00");
+        System.out.println("Press 9: Coffee");
+        System.out.println("Small: $1.00, Medium: $2.00, Large: $3.00");
+        System.out.println("Press 10: Chai Tea");
+        System.out.println("Small: $2.50, Medium: $3.50, Large: $4.50");
+        
+        System.out.println("Press 0 to Exit and get Total ");
+        choice = input.nextInt();
+            
+        }
+        
+        System.out.println("Thank you for stopping by!!!");
+    }
+   
+    
+}
+
+
